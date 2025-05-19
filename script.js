3f1b41349723a1e8195ed8dd589ed8e7e75283e2
Rowdy Rabouw <hi@rowdy.codes>
2025-05-19 22:02:01 +0200

    feat: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function log(text) {
+  logs.innerText += `${text}\r\n`;
+}
+
+const worker = new Worker("worker.js");
+worker.onmessage = ({ data }) => log(`[worker message] ${data}`);
+worker.onerror = ({ message }) => log(`[worker error] ${message}`);
+
+requestDeviceButton.onclick = async () => {
+  const devices = await navigator.hid.requestDevice({
+    filters: [{ vendorId: "0x054c" }],
+  });
+  for (const device of devices) {
+    log(`[window] Requested HID device "${device.productName}"`);
+  }
+};
---
+++ worker.js
@@ -0,0 +1,6 @@
+navigator.hid.onconnect = ({ device }) => {
+  postMessage(`Connected HID device "${device.productName}"`);
+};
+navigator.hid.ondisconnect = ({ device }) => {
+  postMessage(`Disonnected HID device "${device.productName}"`);
+};
